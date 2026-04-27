d4c0e27be8dbcffed9126503504008db47ccce49
fatigued-orca <81499130+fatigued-orca@users.noreply.github.com>
2026-04-27 00:29:05 -0700

    added edit functionality
    Added AccountManager and EditListener to ProfileController.

--- src/ProfileController.java
+++ src/ProfileController.java
@@ -6,16 +6,28 @@ public class ProfileController {
     private ProfileScreen profileView;
     private MainDashBoard dashboardView;
     private UserProfile currentUser;
+    private AccountManager accountManager;
 
-    public ProfileController(ProfileScreen profileView, MainDashBoard dashboardView, UserProfile currentUser) {
+    public ProfileController(ProfileScreen profileView, MainDashBoard dashboardView, UserProfile currentUser, AccountManager accountManager) {
         this.profileView = profileView;
         this.dashboardView = dashboardView;
         this.currentUser = currentUser;
+        this.accountManager = accountManager;
 
         this.profileView.setProfileData(currentUser);
+        this.profileView.addEditListener(new EditListener());
         this.profileView.addBackListener(new BackListener());
     }
+    private class EditListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            EditProfileScreen editScreen = new EditProfileScreen();
+            new EditProfileController(editScreen, profileView, currentUser, accountManager);
 
+            editScreen.setVisible(true);
+            profileView.setVisible(false); 
+        }
+    }
     private class BackListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
